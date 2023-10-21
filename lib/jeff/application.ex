15b2aab64a31f9cd1c3aeee97eb07ee4e0b4fe2f
Emiliano Bovetti <emiliano.bovetti@gmail.com>
2023-10-22 00:14:11 +0200

    feat: start CodeWatcher only on dev environment

--- lib/jeff/application.ex
+++ lib/jeff/application.ex
@@ -1,14 +1,24 @@
 defmodule Jeff.Application do
   use Application
 
+  @dialyzer {:no_underspecs, {:children, 1}}
+
+  @env Mix.env()
+
   @impl Application
   def start(_type, _args) do
-    children = [
-      Jeff.CodeWatcher,
+    opts = [strategy: :one_for_one, name: Jeff.Supervisor]
+    Supervisor.start_link(children(@env), opts)
+  end
+
+  @spec children(atom) :: [Supervisor.child_spec() | {module, any} | module]
+  defp children(:dev) do
+    [Jeff.CodeWatcher | children(:prod)]
+  end
+
+  defp children(_env) do
+    [
       {Plug.Cowboy, plug: JeffWeb.Router, scheme: :http, options: [ip: {0, 0, 0, 0}, port: 8080]}
     ]
-
-    opts = [strategy: :one_for_one, name: Jeff.Supervisor]
-    Supervisor.start_link(children, opts)
   end
 end
